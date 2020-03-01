02a3afcc8fbccab1019d83efd1435a5ae5096e0f
snehagupta01 <sneha2001gupta@gmail.com>
2020-03-01 11:32:27 +0530

    relative layout

--- app/src/main/java/com/example/androidlayouts/MainActivity.java
+++ app/src/main/java/com/example/androidlayouts/MainActivity.java
@@ -18,4 +18,9 @@ public class MainActivity extends AppCompatActivity {
         Intent i=new Intent(this,linearlayout.class);
         startActivity(i);
     }
+    public void openrelativelayout(View view)
+    {
+        Intent i=new Intent(this,relativelayout.class);
+        startActivity(i);
+    }
 }
